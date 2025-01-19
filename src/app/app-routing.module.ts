b02e7f6f13f0dce0355ae4cdadfda5529a5f81aa
Ganesh Kusumbe <gkusumbe7@gmail.com>
2025-01-19 23:33:21 +0530

    added code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StockItemListComponent } from './components/stock-list/stock-list.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { StockManagementComponent } from './components/stock-management/stock-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path : '' , component:HomeComponent },
+    { path : 'stocklist' , component:StockItemListComponent },
+    { path : 'stockmanagement' , component:StockManagementComponent },
+    { path : 'reports' , component:ReportsComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { StockItemListComponent } from './components/stock-list/stock-list.component';
+import { StockManagementComponent } from './components/stock-management/stock-management.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StockItemListComponent,
+    StockManagementComponent,
+    ReportsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
