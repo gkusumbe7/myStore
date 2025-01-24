ebef4b30fd0b654eb7161d1d0b8e28488642f148
Ganesh Kusumbe <gkusumbe7@gmail.com>
2025-01-25 00:50:53 +0530

    added code

--- src/app/components/stock-management/stock-management.component.ts
+++ src/app/components/stock-management/stock-management.component.ts
@@ -8,24 +8,52 @@ import { FormBuilder  , FormGroup  , Validators} from '@angular/forms';
 })
 export class StockManagementComponent {
   
+  // For ADD PRODUT
   saveProductDetails : FormGroup;
   categoryies = ['Fruits & Vegetables' , 'Dairy Products' , 'Personal Care' , 'Grains' ]
-  
+  productstore : any[] = [];  
+
   constructor(private FormBuilder:FormBuilder){
+
     this.saveProductDetails = this.FormBuilder.group({
-        name:['',[Validators.required , Validators.minLength(10)]],
+        name:['',[Validators.required , Validators.minLength(3)]],
         category:['',[Validators.required]],
         price:['',[Validators.required]],
         quantity:['',[Validators.required]]
-    })  
-  }
+    });
 
+  
+    this.addProductsEntry = this.FormBuilder.group({
+        itemCategory:['',[Validators.required]],
+        itemName:['',[Validators.required]],
+        itemPrice:['',[Validators.required]],
+        itemQuantity:['',[Validators.required]]
+    })
+
+
+  }
 
   saveProduct(){
     if(this.saveProductDetails.valid){
       console.log(this.saveProductDetails.value);
-    }else{
+        this.productstore.push(this.saveProductDetails.value);
+        console.log(this.productstore);
+        this.saveProductDetails.reset();
+     }else{
       console.log('Invalid form');
     }
   }
+
+  activeEntry:string = 'addProductEntry';
+
+  showAddProductorProductEntry(entryType:string){
+        this.activeEntry = entryType;
+  }
+
+  // For Production Entry 
+  addProductsEntry : FormGroup;
+  addProductEntry(){}
+
+  
+
 }
